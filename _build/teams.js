7e6be14b1e999a37b18a4c2f1303097ce23681b8
Blynks <eremtek@gmail.com>
2016-06-04 14:06:18 -0400

    commented out team.js

--- _build/teams.js
+++ _build/teams.js
@@ -10,20 +10,20 @@ Show games that can be bet on - outputs gameID, date, time, team names.
 */
 console.log("teams file");
 
-controller.setupWebserver(3000, function(err, webserver) {
-    controller.createWebhookEndpoints(webserver);
-    console.log(webserver);
-});
-
-controller.on('games', function(bot, message) {
-    // check message.command
-    // and maybe message.text...
-    // use EITHER replyPrivate or replyPublic...
-    bot.replyPrivate(message, 'This is a private reply to the ' + message.command + ' slash command!');
-
-    // and then continue to use replyPublicDelayed or replyPrivateDelayed
-    bot.replyPublicDelayed(message, 'This is a public reply to the ' + message.command + ' slash command!');
-
-    bot.replyPrivateDelayed(message, ':dash:');
-
-});
+// controller.setupWebserver(3000, function(err, webserver) {
+//     controller.createWebhookEndpoints(webserver);
+//     console.log(webserver);
+// });
+//
+// controller.on('games', function(bot, message) {
+//     // check message.command
+//     // and maybe message.text...
+//     // use EITHER replyPrivate or replyPublic...
+//     bot.replyPrivate(message, 'This is a private reply to the ' + message.command + ' slash command!');
+//
+//     // and then continue to use replyPublicDelayed or replyPrivateDelayed
+//     bot.replyPublicDelayed(message, 'This is a public reply to the ' + message.command + ' slash command!');
+//
+//     bot.replyPrivateDelayed(message, ':dash:');
+//
+// });
