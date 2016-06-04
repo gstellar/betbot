83e165c9974b2339eec1c918a7116a50156530a2
Stella Lee <stellajhlee@hotmail.com>
2016-06-04 12:43:40 -0400

    teams comments

--- _build/teams.js
+++ _build/teams.js
@@ -1,1 +1,12 @@
+/*
+
+Show games that can be bet on - outputs gameID, date, time, team names.
+
+# USAGE
+    /games                          -   shows all games
+    /games [nhl, nfl, nba]          -   shows the current games for the specified league
+    /games [current, upcoming]      -   shows current or upcoming games
+
+*/
+
 console.log("teams file");
