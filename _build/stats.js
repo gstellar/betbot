0c6617f61b06cd71322229fc2367c41fa6da231e
Stella Lee <stellajhlee@hotmail.com>
2016-06-04 12:44:55 -0400

    stats comments

--- _build/stats.js
+++ _build/stats.js
@@ -1,1 +1,11 @@
+/*
+
+View team and game stats - outputs favoured team, number of current bets per team.
+
+# USAGE
+    /stats [gameID]                 - shows the stats for the specified game
+    /stats [team]                   - shows the stats for the specified team
+
+*/
+
 console.log("stats file");
