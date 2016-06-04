d911a42b52463fd92d883adb08977a43c8a8d713
Stella Lee <stellajhlee@hotmail.com>
2016-06-04 12:38:45 -0400

    add bets comments

--- _build/bets.js
+++ _build/bets.js
@@ -1,1 +1,16 @@
+/*
+
+View or place bets
+
+# QUESTIONS
+Should you be able to revoke bets after placing?
+How can you change your bet? 
+
+# USAGE
+    /bet                        -   shows your bets
+    /bet [all]                  -   shows all bets
+    /bet [gameID] [team name]   -   place your bet
+
+*/
+
 console.log("bets file");
