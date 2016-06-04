46ca62fca796edb3a35975c98408741e3d4997e5
Stella Lee <stellajhlee@hotmail.com>
2016-06-04 12:40:16 -0400

    add leaderboard comments

--- _build/leaderboard.js
+++ _build/leaderboard.js
@@ -1,1 +1,12 @@
+/*
+
+View leaderboard - username, rank, number of points
+
+# USAGE
+    /leaderboard                        -   shows the whole leaderboard
+    /leaderboard [username]             -   shows the rank for the specified user
+    /leaderboard me                     -   shows your rank
+
+*/
+
 console.log("leaderboard file");
