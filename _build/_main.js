e6108519e053d9193157ccc6e10608895cef1eb5
Alex <alex@alisterdev.com>
2016-06-04 16:34:06 -0400

    app integration

--- _build/_main.js
+++ _build/_main.js
@@ -1,28 +1,41 @@
 var Botkit = require('../node_modules/botkit/lib/Botkit.js');
 
-if (!process.env.token) {
-  console.log('Error: Specify token in environment');
-  process.exit(1);
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.token) {
+    console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
+    process.exit(1);
 }
 
-//needs to be global
-controller = Botkit.slackbot({
- debug: false
+var config = {}
+if (process.env.MONGOLAB_URI) {
+    var BotkitStorage = require('botkit-storage-mongo');
+    config = {
+        storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
+    };
+} else {
+    config = {
+        json_file_store: './db_slackbutton_slash_command/',
+    };
+}
+
+controller = Botkit.slackbot(config).configureSlackApp({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  scopes: ['commands'],
 });
 
-controller.spawn({
-  token: process.env.token
-}).startRTM(function(err) {
-  if (err) {
-    throw new Error(err);
-  }
+controller.setupWebserver(process.env.PORT, function (err, webserver) {
+    controller.createWebhookEndpoints(controller.webserver);
+    controller.createOauthEndpoints(controller.webserver, function (err, req, res) {
+        if (err) {
+            res.status(500).send('ERROR: ' + err);
+        } else {
+            res.send('Success!');
+        }
+    });
 });
 
-// Example 1
-controller.hears(['hello','hi'],['direct_message','direct_mention','mention'],function(bot,message) {
-    bot.reply(message,"Sup, y'all.");
-});
-
+require('./bot.js');
+require('./slashCommands.js');
 require('./bets.js');
 require('./leaderboard.js');
 require('./stats.js');
--- _build/slashCommands.js
+++ _build/slashCommands.js
@@ -8,27 +8,25 @@ Show games that can be bet on - outputs gameID, date, time, team names.
     /games [current, upcoming]      -   shows current or upcoming games
 
 */
-console.log("teams file");
-
 
 controller.on('slash_command', function (slashCommand, message) {
 
     console.log("Received command: " + message.command);
     switch (message.command) {
         case "/echo":
-            test();
+            test(slashCommand, message);
             break;
         case "/games":
-            games();
+            games(slashCommand, message);
             break;
         case "/bet":
-            bet();
+            bet(slashCommand, message);
             break;
         case "/stats":
-            stats();
+            stats(slashCommand, message);
             break;
         case "/leaderboard":
-            leaderboard();
+            leaderboard(slashCommand, message);
             break;
 
         default:
@@ -37,7 +35,7 @@ controller.on('slash_command', function (slashCommand, message) {
 
 });
 
-function games() {
+function games(slashCommand, message) {
     console.log("games command");
     
     slashCommand.replyPublic(message, "1", function() {
@@ -46,7 +44,7 @@ function games() {
     
 }
 
-function bet() {
+function bet(slashCommand, message) {
     console.log("bet command");
     
     slashCommand.replyPublic(message, "1", function() {
@@ -54,15 +52,51 @@ function bet() {
     });
 }
 
-function stats() {
+function stats(slashCommand, message) {
     console.log("stats command");
     
-    slashCommand.replyPublic(message, "1", function() {
-        slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+    var attachments = [];
+    var attachment = {
+        title: "19:45 into the 2nd Period",
+        title_link: 'http://nhl.com',
+        color: '#2f5997',
+        fields: [
+            {
+                title: "Penguins",
+                value: "1",
+                short: true
+            },
+            {
+                title:"Sharks",
+                value: "2",
+                short: true
+            }
+        ],
+        short: false,
+    };
+
+    attachment.fields.push({
+        label: 'Field',
+        value: "Penguins lead the series 2 - 0",
+        short: false,
     });
+
+    attachments.push(attachment);
+
+
+    slashCommand.replyPublic(message, {
+        attachments: attachments,
+    },function(err,resp) {
+        console.log(err,resp);
+    });
+    
+    
+    // slashCommand.replyPublic(message, "1", function() {
+    //     slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+    // });
 }
 
-function leaderboard() {
+function leaderboard(slashCommand, message) {
     console.log("leaderboard command");
     
     slashCommand.replyPublic(message, "1", function() {
@@ -70,23 +104,23 @@ function leaderboard() {
     });
 }
 
-function test() {
+function test(slashCommand, message) {
     console.log("test command");
 
     //handle the `/echo` slash command. We might have others assigned to this app too!
             // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
             // Otherwise just echo back to them what they sent us.
 
-            // but first, let's make sure the token matches!
-            if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
-            console.log("passed token");
-            // if no text was supplied, treat it as a help command
-            if (message.text === "" || message.text === "help") {
-                slashCommand.replyPrivate(message,
-                    "I echo back what you tell me. " +
-                    "Try typing `/echo hello` to see.");
-                return;
-            }
+            // // but first, let's make sure the token matches!
+            // if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
+            // console.log("passed token");
+            // // if no text was supplied, treat it as a help command
+            // if (message.text === "" || message.text === "help") {
+            //     slashCommand.replyPrivate(message,
+            //         "I echo back what you tell me. " +
+            //         "Try typing `/echo hello` to see.");
+            //     return;
+            // }
 
             // If we made it here, just echo what the user typed back at them
             //TODO You do it!
