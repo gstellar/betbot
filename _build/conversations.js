34add68fd65bb5611d40480db7f025baae9f94cc
Stella Lee <stellajhlee@hotmail.com>
2016-06-04 14:42:11 -0400

    shutdown

--- _build/conversations.js
+++ _build/conversations.js
@@ -26,3 +26,29 @@ controller.hears(['show leaderboard'],['direct_message','direct_mention','mentio
     bot.reply(message,"TOO BAD!");
     });
 });
+
+controller.hears(['shutdown'],['direct_message','direct_mention'],function(bot, message) {
+
+    bot.startConversation(message,function(err, convo) {
+        convo.ask('Are you sure you want me to shutdown?',[
+            {
+                pattern: bot.utterances.yes,
+                callback: function(response, convo) {
+                    convo.say('Bye for now!');
+                    convo.next();
+                    setTimeout(function() {
+                        process.exit();
+                    },3000);
+                }
+            },
+        {
+            pattern: bot.utterances.no,
+            default: true,
+            callback: function(response, convo) {
+                convo.say('Good choice! :innocent:');
+                convo.next();
+            }
+        }
+        ]);
+    });
+});
