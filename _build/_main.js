b798a1532a0f36ede1ae5a64a26d2b19c9016767
Alex <alex@alisterdev.com>
2016-06-04 12:23:27 -0400

    init commit

---
+++ _build/_main.js
@@ -0,0 +1,30 @@
+var Botkit = require('../node_modules/botkit/lib/Botkit.js');
+
+if (!process.env.token) {
+  console.log('Error: Specify token in environment');
+  process.exit(1);
+}
+
+var controller = Botkit.slackbot({
+ debug: false
+});
+
+controller.spawn({
+  token: process.env.token
+}).startRTM(function(err) {
+  if (err) {
+    throw new Error(err);
+  }
+});
+
+// Example 1
+controller.hears(['hello','hi'],['direct_message','direct_mention','mention'],function(bot,message) {
+    bot.reply(message,"Hello.");
+});
+
+require('./bets.js');
+require('./leaderboard.js');
+require('./stats.js');
+require('./teams.js');
+
+
