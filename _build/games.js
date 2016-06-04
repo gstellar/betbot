43c144fd08375289aa0dd8cb7423572ddc87c195
Stella Lee <stellajhlee@hotmail.com>
2016-06-04 17:00:09 -0400

    comment out games test code

--- _build/games.js
+++ _build/games.js
@@ -33,61 +33,61 @@ controller.hears(['upcoming'],['direct_message','direct_mention'],function(bot,m
     });
 });
 
-function games() {
-    console.log("games command");
+// function games() {
+//     console.log("games command");
     
-    slashCommand.replyPublic(message, "1", function() {
-        slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
-    });
+//     slashCommand.replyPublic(message, "1", function() {
+//         slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+//     });
     
-}
+// }
 
-function bet() {
-    console.log("bet command");
+// function bet() {
+//     console.log("bet command");
     
-    slashCommand.replyPublic(message, "1", function() {
-        slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
-    });
-}
+//     slashCommand.replyPublic(message, "1", function() {
+//         slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+//     });
+// }
 
-function stats() {
-    console.log("stats command");
+// function stats() {
+//     console.log("stats command");
     
-    slashCommand.replyPublic(message, "1", function() {
-        slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
-    });
-}
+//     slashCommand.replyPublic(message, "1", function() {
+//         slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+//     });
+// }
 
-function leaderboard() {
-    console.log("leaderboard command");
+// function leaderboard() {
+//     console.log("leaderboard command");
     
-    slashCommand.replyPublic(message, "1", function() {
-        slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
-    });
-}
+//     slashCommand.replyPublic(message, "1", function() {
+//         slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+//     });
+// }
 
-function test() {
-    console.log("test command");
+// function test() {
+//     console.log("test command");
 
-    //handle the `/echo` slash command. We might have others assigned to this app too!
-            // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
-            // Otherwise just echo back to them what they sent us.
+//     //handle the `/echo` slash command. We might have others assigned to this app too!
+//             // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
+//             // Otherwise just echo back to them what they sent us.
 
-            // but first, let's make sure the token matches!
-            if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
-            console.log("passed token");
-            // if no text was supplied, treat it as a help command
-            if (message.text === "" || message.text === "help") {
-                slashCommand.replyPrivate(message,
-                    "I echo back what you tell me. " +
-                    "Try typing `/echo hello` to see.");
-                return;
-            }
+//             // but first, let's make sure the token matches!
+//             if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
+//             console.log("passed token");
+//             // if no text was supplied, treat it as a help command
+//             if (message.text === "" || message.text === "help") {
+//                 slashCommand.replyPrivate(message,
+//                     "I echo back what you tell me. " +
+//                     "Try typing `/echo hello` to see.");
+//                 return;
+//             }
 
-            // If we made it here, just echo what the user typed back at them
-            //TODO You do it!
-            slashCommand.replyPublic(message, "1", function() {
-                slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
-            });          
-}
+//             // If we made it here, just echo what the user typed back at them
+//             //TODO You do it!
+//             slashCommand.replyPublic(message, "1", function() {
+//                 slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+//             });          
+// }
 
