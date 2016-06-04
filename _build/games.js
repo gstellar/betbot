515b8e2d85bf135a1d7927846f18cc5f56852efb
Blynks <eremtek@gmail.com>
2016-06-04 16:36:36 -0400

    added upcoming

--- _build/games.js
+++ _build/games.js
@@ -7,6 +7,7 @@ Show games that can be bet on - outputs gameID, date, time, team names.
     /games [nhl, nfl, nba]          -   shows the current games for the specified league
     /games [current, upcoming]      -   shows current or upcoming games
 
+    keyword: upcoming               -   shows a list of upcoming games
 */
 console.log("games file");
 
@@ -21,4 +22,30 @@ controller.on('games', function(bot, message) {
 
     bot.replyPrivateDelayed(message, ':dash:');
 
+});
+
+controller.hears(['upcoming'],['direct_message','direct_mention'],function(bot,message) {
+
+    var attachments = [];
+    var attachment = {
+        title: "Game 4 begins June 6, 2016",
+        title_link: 'http://nhl.com',
+        color: '#ff6600',
+        fields: [],
+        short: false,
+    };
+
+    attachment.fields.push({
+        label: 'Field',
+        value: "Sharks lead the series 2 - 0",
+        short: false,
+    });
+
+    attachments.push(attachment);
+
+    bot.reply(message,{
+        attachments: attachments,
+    },function(err,resp) {
+        console.log(err,resp);
+    });
 });
