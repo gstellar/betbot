142b224551fd3e4fbd122b8089bdc5409051fad7
Darrel <ddayy.guerrero@hotmail.com>
2016-06-05 02:00:37 -0400

    Filter upcoming dates, add date formatter

--- _build/utils.js
+++ _build/utils.js
@@ -3,12 +3,18 @@ gbotUtil = (function () {
         createScoresAttachment: CreateScoresAttachment,
         createUpcomingGamesAttachment: createUpcomingGamesAttachment
     };
-
+    // PRIVATE METHODS
+    function formatDate(date){
+        date = date.replace(/\s\s+/g, ' ');
+        var dateElements = date.split(' ');
+        return dateElements[0].substring(0, 3) + " " + dateElements[1] + " " + dateElements[2];
+    }
+    
+    // PUBLIC METHODS
     function CreateScoresAttachment(games) {
         var attachments = [];
         var fields = [];
         games.forEach(function (e) {
-            console.log(e);
             fields.push({
                 title: ":" + e.awayTeam.toString() + ": " + e.awayTeam + " (" + e.awayScore + ") " + " @ " + ":" + e.homeTeam.toString() + ": " + e.homeTeam + " (" + e.homeScore + ")"
             });
@@ -29,11 +35,16 @@ gbotUtil = (function () {
         var attachments = [];
         var fields = [];
         games.forEach(function (e) {
-            fields.push({
-                title: "Game " + e.gameNumber + " begins at " + e.date,
-                color: '#ff6600',
-                short: false,
-            });
+            var newDate = Date.parse(formatDate(e.date));
+            var today = new Date();
+            var isUpcoming = newDate >= today.getTime();
+            if(isUpcoming){
+                fields.push({
+                    title: "Game " + e.gameNumber + " begins at " + e.date,
+                    color: '#ff6600',
+                    short: false,
+                });
+            }
         });
 
         var attachment = {
