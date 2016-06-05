90dd220c2928b8cdd094510cedcceb3e71487342
Stella Lee <stellajhlee@hotmail.com>
2016-06-05 00:23:55 -0400

    reformat games

--- _build/games.js
+++ _build/games.js
@@ -23,109 +23,141 @@ games = function games(slashCommand, message) {
     });
 }
 
-    /** Called from slashCommands.js */
-    games = function games(slashCommand, message) {
+/** Called from slashCommands.js */
+games = function games(slashCommand, message) {
 
-        var gameType = message.text;
-        myCommand = slashCommand;
-        myMessage = message;
+    var gameType = message.text;
+    myCommand = slashCommand;
+    myMessage = message;
 
-        if (gameType == "nba") {
-            gbotSportsAPI.getNbaGames();
-        }
-        else if (gameType == "nhl") {
-            gbotSportsAPI.getNhlGames();
-        }
-        else {
-            slashCommand.replyPublic(message, "Use /games nba or /games nhl to get upcoming games of the league", function () { });
-        }
-
+    if (gameType == "nba") {
+        gbotSportsAPI.getNbaGames();
+    }
+    else if (gameType == "nhl") {
+        gbotSportsAPI.getNhlGames();
+    }
+    else {
+        slashCommand.replyPublic(message, "Use /games nba or /games nhl to get upcoming games of the league", function () { });
     }
 
-    // Listening methods
-    controller.hears(['upcoming'], ['direct_message', 'direct_mention'], function (bot, message) {
+}
 
-        var games = [
-            {
-                gameNumber: 4, date: "June 26, 2016", winningTeam: "Sharks :sshar:", score1: 2, score2: 1
-            },
-            {
-                gameNumber: 5, date: "June 16, 2016", winningTeam: "Penguins :ppens:", score1: 3, score2: 2
-            }
-        ];
+// Listening methods
+controller.hears(['upcoming'], ['direct_message', 'direct_mention'], function (bot, message) {
 
-        // console.log(games);
-
-        var responseObj = getUpcomingGames(games);
-        if (responseObj) {
-            bot.reply(message, responseObj, function (err, resp) {
-                console.log(err, resp);
-            });
+    var games = [
+        {
+            gameNumber: 4, date: "June 26, 2016", winningTeam: "Sharks :sshar:", score1: 2, score2: 1
+        },
+        {
+            gameNumber: 5, date: "June 16, 2016", winningTeam: "Penguins :ppens:", score1: 3, score2: 2
         }
-    });
+    ];
 
-    controller.hears(['all'], ['direct_message', 'direct_mention'], function (bot, message) {
+    // console.log(games);
 
-        var games = [
-            {
-                gameNumber: 4, date: "June 26, 2016", winningTeam: "Panthers :cavs:", score1: 2, score2: 1
-            },
-            {
-                gameNumber: 5, date: "June 16, 2016", winningTeam: "Penguins :ppens:", score1: 3, score2: 2
-            }
-        ];
+    var responseObj = getUpcomingGames(games);
+    if (responseObj) {
+        bot.reply(message, responseObj, function (err, resp) {
+            console.log(err, resp);
+        });
+    }
+});
 
+controller.hears(['all'], ['direct_message', 'direct_mention'], function (bot, message) {
 
-        var responseObj = getAllGames(games);
-        if (responseObj) {
-            bot.reply(message, responseObj, function (err, resp) {
-                console.log(err, resp);
-            });
+    var games = [
+        {
+            gameNumber: 4, date: "June 26, 2016", winningTeam: "Panthers :cavs:", score1: 2, score2: 1
+        },
+        {
+            gameNumber: 5, date: "June 16, 2016", winningTeam: "Penguins :ppens:", score1: 3, score2: 2
         }
-    });
+    ];
 
-    controller.hears(['current'], ['direct_message', 'direct_mention'], function (bot, message) {
 
-        var games = [
-            {
-                gameNumber: 4, date: "June 26, 2016", winningTeam: "Panthers :cavs:", score1: 2, score2: 1
-            },
-            {
-                gameNumber: 5, date: "June 16, 2016", winningTeam: "Penguins :ppens:", score1: 3, score2: 2
-            }
-        ];
+    var responseObj = getAllGames(games);
+    if (responseObj) {
+        bot.reply(message, responseObj, function (err, resp) {
+            console.log(err, resp);
+        });
+    }
+});
 
-        var responseObj = getCurrentGames(games);
-        if (responseObj) {
-            bot.reply(message, responseObj, function (err, resp) {
-                console.log(err, resp);
-            });
+controller.hears(['current'], ['direct_message', 'direct_mention'], function (bot, message) {
+
+    var games = [
+        {
+            gameNumber: 4, date: "June 26, 2016", winningTeam: "Panthers :cavs:", score1: 2, score2: 1
+        },
+        {
+            gameNumber: 5, date: "June 16, 2016", winningTeam: "Penguins :ppens:", score1: 3, score2: 2
         }
-    });
+    ];
 
-    // Getters
-    function getUpcomingGames(games) {
+    var responseObj = getCurrentGames(games);
+    if (responseObj) {
+        bot.reply(message, responseObj, function (err, resp) {
+            console.log(err, resp);
+        });
+    }
+});
 
-        var attachments = [];
-        var attachment = {
-            fallback: "Upcoming Games",
-            title: "Upcoming Games",
-            fields: [],
+// Getters
+function getUpcomingGames(games) {
+
+    var attachments = [];
+    var attachment = {
+        fallback: "Upcoming Games",
+        title: "Upcoming Games",
+        fields: [],
+        short: false,
+    }
+
+    games.forEach(function (game) {
+        //if (game["date"] > today) { // NEED TO DECIDE WHEN "UPCOMING" IS
+
+        attachment.fields.push({
+            title: "Game " + game.gameNumber + " begins at " + game.date,
+            title_link: 'http://nhl.com',
+            color: '#ff6600',
             short: false,
-        }
+        });
 
-        games.forEach(function (game) {
-            //if (game["date"] > today) { // NEED TO DECIDE WHEN "UPCOMING" IS
+        attachment.fields.push({
+            title: "Game " + game.gameNumber + " begins at " + game.date,
+            title_link: 'http://nhl.com',
+            color: '#ff6600',
+            short: false,
+        });
 
+        attachment.fields.push({
+            label: 'Field',
+            value: game.winningTeam + " lead the series " + game.home + " - " + game.score2,
+            short: false,
+        });
+        //}
+    }, this);
+
+    attachments.push(attachment);
+
+    return { attachments: attachments };
+
+}
+
+function getCurrentGames(games) {
+    var attachments = [];
+    var attachment = {
+        fallback: "Upcoming Games",
+        title: "Upcoming Games",
+        fields: [],
+        short: false,
+    }
+
+    games.forEach(function (game) {
+        if (game["date"] == today) { // NEED TO DECIDE WHEN "CURRENT" IS
             attachment.fields.push({
-                title: "Game " + game.gameNumber + " begins at " + game.date,
-                title_link: 'http://nhl.com',
-                color: '#ff6600',
-                short: false,
-            });
-
-            attachment.fields.push({
-                title: "Game " + game.gameNumber + " begins at " + game.date,
+                title: "Game " + game.gameNumber + " begins " + game.date,
                 title_link: 'http://nhl.com',
                 color: '#ff6600',
                 short: false,
@@ -133,68 +165,35 @@ games = function games(slashCommand, message) {
 
             attachment.fields.push({
                 label: 'Field',
-                value: game.winningTeam + " lead the series " + game.home + " - " + game.score2,
+                value: game.winningTeam + " lead the series " + game.score1 + " - " + game.score2,
                 short: false,
             });
-            //}
-        }, this);
-
-        attachments.push(attachment);
-
-        return { attachments: attachments };
-
-    }
-
-    function getCurrentGames(games) {
-        var attachments = [];
-        var attachment = {
-            fallback: "Upcoming Games",
-            title: "Upcoming Games",
-            fields: [],
-            short: false,
         }
+    }, this);
 
-        games.forEach(function (game) {
-            if (game["date"] == today) { // NEED TO DECIDE WHEN "CURRENT" IS
-                attachment.fields.push({
-                    title: "Game " + game.gameNumber + " begins " + game.date,
-                    title_link: 'http://nhl.com',
-                    color: '#ff6600',
-                    short: false,
-                });
+    attachments.push(attachment);
 
-                attachment.fields.push({
-                    label: 'Field',
-                    value: game.winningTeam + " lead the series " + game.score1 + " - " + game.score2,
-                    short: false,
-                });
-            }
-        }, this);
+    return { attachments: attachments };
+}
 
-        attachments.push(attachment);
-
-        return { attachments: attachments };
+function getAllGames(games) {
+    var attachments = [];
+    var attachment = {
+        fallback: "All Games",
+        title: "All Games",
+        fields: [],
+        short: false,
     }
-
-    function getAllGames(games) {
-        var attachments = [];
-        var attachment = {
-            fallback: "All Games",
-            title: "All Games",
-            fields: [],
+    games.forEach(function (game) {
+        attachment.fields.push({
+            title: "GameID" + game.gameID + ". " + game.otherTeam + " vs " + game.favouredTeam + " @ " + game.date,
+            title_link: 'http://nhl.com',
+            color: '#ff6600',
             short: false,
-        }
-        games.forEach(function (game) {
-            attachment.fields.push({
-                title: "GameID" + game.gameID + ". " + game.otherTeam + " vs " + game.favouredTeam + " @ " + game.date,
-                title_link: 'http://nhl.com',
-                color: '#ff6600',
-                short: false,
-            });
-        }, this);
+        });
+    }, this);
 
-        attachments.push(attachment);
+    attachments.push(attachment);
 
-        return { attachments: attachments };
-    }
-   
+    return { attachments: attachments };
+}
